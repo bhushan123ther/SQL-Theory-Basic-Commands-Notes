73b73fc7b2f260be881fcbfa3a65511d964c1624
Bhushan  Ther <87998214+bhushan123ther@users.noreply.github.com>
2023-04-09 12:34:32 +0530

    Add files via upload

---
+++ Basic important difference Relational Vs Non-Relational and SQL Vs No-SQL.sql
@@ -0,0 +1,43 @@
+/*
+=> Relational Vs Non-Relational
+
+Relational databases are based on the relational model with strict schema requirements,
+while non-relational databases use various data models without strict schema requirements.
+
+Relational databases are vertically scalable,
+while non-relational databases are horizontally scalable.
+
+Relational databases use SQL as a standard query language,
+while non-relational databases typically use proprietary query languages.
+
+Relational databases offer ACID guarantees,
+while non-relational databases may offer eventual consistency.
+
+Relational databases are commonly used for applications that require data integrity and strict relationships between tables,
+while non-relational databases are commonly used for handling large volumes of unstructured or semi-structured data.
+*/
+
+/*
+
+=> SQL Vs NO-SQL
+
+->SQL database are relational
+->NO-SQL database are non-relational
+
+vertical scalability means adding more resources to a single server to handle increased workload, while horizontal scalability means adding more servers to handle the workload in a distributed fashion.
+
+->SQL database are vertically scalable
+->NO-SQL database are horizantally scalable
+
+->SQL database have fixed or static or predefined schema.
+->NO-SQL database have dynamic schema.
+
+->SQL database are best suited for complex queries.
+->NO-SQL database are not so good for complex queries.
+
+->SQL database offer ACID gurantees, while NoSQL database typically offer eventual consistency.
+   
+->SQL databases are MYSQL,PostgreSQL,Oracle,MS-SQL Server,etc.
+->NO-SQL databases are MongoDB,GraphQL,HBase,Neo4j,Cassandra,etc.
+
+*/
