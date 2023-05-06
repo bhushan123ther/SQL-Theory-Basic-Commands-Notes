dde8ab5d2f036fa8ccc9d826c6d72e13a3da94b5
Bhushan  Ther <87998214+bhushan123ther@users.noreply.github.com>
2023-05-06 23:55:24 +0530

    Add files via upload

---
+++ Important Syntax/Imp Syntax.sql
@@ -0,0 +1,31 @@
+-- Syntax of all important queries or commands
+
+-- ALTER
+-- The ALTER command is used to modify the structure of an existing database table. Here's the basic syntax:
+-- alter table table_name action; (e.g. ADD COLUMN, DROP COLUMN, RENAME COLUMN, etc.)
+
+-- UPDATE
+-- The UPDATE command is used to modify existing records in a database table. Here's the basic syntax:
+-- UPDATE table_name SET column1 = value1, column2 = value2 WHERE condition;
+
+-- DELETE
+-- The DELETE command is used to remove records from a database table. Here's the basic syntax:
+-- DELETE FROM table_name WHERE condition;
+
+-- TRUNCATE
+-- The TRUNCATE command is used to remove all records from a database table. Here's the basic syntax:
+-- TRUNCATE TABLE table_name;
+
+-- DROP
+-- The DROP command is used to remove an entire database or database object (e.g. a table) from the MySQL server. Here's the basic syntax:
+-- DROP object_type object_name; (e.g. database,table)
+
+-- CREATE
+-- The CREATE command is used to create a new database or database object (e.g. a table) in MySQL. Here's the basic syntax:
+-- CREATE object_type object_name (column1 datatype1, column2 datatype2, ...); (e.g. DATABASE, TABLE, etc.)
+
+-- INSERT
+-- The INSERT command is used to insert new records into a database table. Here's the basic syntax:
+-- INSERT INTO table_name (column1, column2, ...) VALUES (value1, value2, ...);
+
+
